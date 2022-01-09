1ab6b38aa49c70de538fd00f80990376c0f155ad
Gabriel de Oliveira Baptista @gabrielsinformatica <88345156+gabrielsinformatica@users.noreply.github.com>
2022-01-09 12:10:01 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,21 +1,23 @@
 let botao = document.querySelector(".btn-submit");
 
 botao.addEventListener("click", function() {
+    let nomeUser = document.querySelector("#userName").value;
     let email = document.querySelector("#email").value;
     let senha = document.querySelector("#senha").value;
 
-    console.log(email, senha);
+    console.log(nomeUser, email, senha);
 
     let usuario1 = {
+        nomeUser: "admin",
         email: "admin@linkerdigital.com",
-        senha: "linkerdigital"
+        senha: "linkerdigital",
     }
 
-    if(email === "" || senha === "") {
+    if(nomeUser === "" || email === "" || senha === "") {
         alert("Preencha os campos corretamente!");
         document.querySelector("#email").value = "";
         document.querySelector("#senha").value = "";
-    } else if(usuario1.email === "admin@linkerdigital.com" && usuario1.senha === "linkerdigital") {
+    } else if(usuario1.nomeUser === "admin" && usuario1.email === "admin@linkerdigital.com" && usuario1.senha === "linkerdigital") {
         document.querySelector("#sera").innerHTML = "Acesso permitido! <a href='painel.html'>Clique aqui</a> para acessar seu painel de controle!";
     }
 });
